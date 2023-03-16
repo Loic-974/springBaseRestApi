dcad84f92f6b34cb780f1cc313e369003c924358
Loic <loic.rabat@live.fr>
2023-03-16 13:13:26 +0400

    Master - First Commit

---
+++ src/test/java/com/expert/baseRestApi/BaseRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expert.baseRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
